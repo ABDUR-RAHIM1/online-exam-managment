2196bff74d78e531991bcc1ad66e7ad3f392a3b8
ABDUR-RAHIM1 <abdurrahim88557@gmail.com>
2024-12-07 12:51:56 +0600

    added test route

---
+++ app/(home)/test/page.js
@@ -0,0 +1,65 @@
+"use client"
+import { API_URL } from '@/app/constans/constans';
+import React, { useState } from 'react'
+
+export default function page() {
+
+    const [file, setFile] = useState("")
+
+    const handleChange = async (e) => {
+        const fileValue = e.target.files[0];
+        console.log(fileValue)
+        setFile(fileValue)
+
+        const formData = new FormData();
+        formData.append("photo", fileValue)
+
+        try {
+            const response = await fetch(`${API_URL + "/test/upload"}`, {
+                method: "POST",
+                // headers: {
+                //     "Content-type": "application/json"
+                // },
+                body: formData
+            });
+
+            const result = await response.json();
+            console.log(result)
+        } catch (error) {
+            console.log("client error", error)
+        }
+
+    }
+
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+
+    //     const formData = new FormData();
+    //     formData.append("photo", file)
+
+    //     try {
+    //         const response = await fetch(`${API_URL + "/test/upload"}`, {
+    //             method: "POST",
+    //             // headers: {
+    //             //     "Content-type": "application/json"
+    //             // },
+    //             body: formData
+    //         });
+
+    //         const result = await response.json();
+    //         console.log(result)
+    //     } catch (error) {
+    //         console.log("client error", error)
+    //     }
+
+    // }
+
+    return (
+        <form   className=' w-full min-h-screen flex items-center justify-center flex-col'>
+            <input onChange={handleChange} type="file" className='input' />
+            <input type="text" name='email' onChange={handleChange} className='input' />
+            <input type="text" name='email' onChange={handleChange} className='input' />
+            <button className='loginBtn'>Submit</button>
+        </form>
+    )
+}
